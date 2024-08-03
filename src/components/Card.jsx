63e2eda8aa4ec4a727ccda4dc2e0f1cc4a2ff87d
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-08-03 22:07:14 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,7 +17,7 @@ function DetailPage() {
         <p className='fs-4 p-2 text-white'>Details Page For Post With ID {post.id}</p>
         <div className="container-fluid d-flex justify-content-start">
         <div className="card text-white bg-transparent border border-0 fs-4 justified-text" style={{ width: '50%' }}>
-            <img src={`https://picsum.photos/200?random=${post.id}`} className="det-img" alt="image"  height={300} width={300}/>
+            <img src={`https://picsum.photos/200?random=${post.id}`} className="det-img" height={300} width={300}/>
             <div className="card-body">
                 <p className=''>User ID : {post.id}</p>
                 <p className="card-title">Title : {post.title}</p>
