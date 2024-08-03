0584ab814df17d7fcbe791f8323cea787447934f
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-08-03 21:58:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; //check this in hashrouter
+import HomePage from './components/Home';
+import DetailPage from './components/Card';
+
+
+function App() {
+  return (
+    <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/item/:id" element={<DetailPage />} />
+            </Routes>
+        </Router>
+
+  );
+}
+
+export default App;
